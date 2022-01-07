df4a69887cd1ae91361a9d0473b6b89114f6afcd
Lucas Freitas <lucas.freitas@captalys.com.br>
2022-01-07 11:10:33 -0300

    primeio commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# project/app/main.py
+
+from fastapi import FastAPI, Depends
+
+from app.config import Settings, get_settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
